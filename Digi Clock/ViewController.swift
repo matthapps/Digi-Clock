960a5148c7b0e7ba9a8a7891f05e4ddc61f5bcec
Admin <admin@admins-macbook-pro.local>
2018-03-26 20:27:18 -0700

    Initial Commit

---
+++ Digi Clock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Digi Clock
+//
+//  Created by Admin on 3/26/18.
+//  Copyright © 2018 Matthew Heinrichs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
